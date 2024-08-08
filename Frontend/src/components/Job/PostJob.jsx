87313c449a02518cafeedd820b7f5313406ab561
Aashish Rawat <95233342+aashishrawat6@users.noreply.github.com>
2024-08-08 23:31:52 +0530

    Update PostJob.jsx url

--- Frontend/src/components/Job/PostJob.jsx
+++ Frontend/src/components/Job/PostJob.jsx
@@ -36,7 +36,7 @@ const PostJob = () => {
     //
     await axios
       .post(
-        "http://localhost:4000/api/v1/job/post",
+        "https://hr-management-backend-hboj.onrender.com/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
